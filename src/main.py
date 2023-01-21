88f75830954428644513cf1f938215d556b540cb
Andrew Hwang <hwanga097@gmail.com>
2023-01-20 21:05:14 -0800

    updated main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
 import discord
+from discord.ext import commands
 from bot_cogs.music import Music
 from bot_cogs.general import General
+import os
+
+bot = commands.Bot(command_prefix='/')
+bot.add_cog(Music(bot))
+bot.add_cog(General(bot))
+
+bot.run(os.getenv("TOKEN"))

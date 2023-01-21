4945475ce25b661302a39045002528a822289637
Andrew Hwang <54608684+AndrewHwang97@users.noreply.github.com>
2023-01-20 17:10:25 -0800

    initial setup of bot

---
+++ src/bot_abstractions/constants.py
@@ -0,0 +1,13 @@
+
+class YDLConstants:
+    BESTAUDIO = 'bestaudio'
+    FORMAT = 'format'
+
+class FfmpegConstants:
+    BEFORE_OPTIONS = 'before_options'
+    BEFORE_OPTIONS_SETTINGS = '-reconnect 1 -reconnected_streamed 1 -reconnect_delay_max 5'
+    OPTIONS = 'options'
+    VN = '-vn'
+
+class Constants:
+    SOURCE = 'source'
---
+++ src/main.py
@@ -0,0 +1,3 @@
+import discord
+from bot_cogs.music import Music
+from bot_cogs.general import General

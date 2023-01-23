41ca41d253b5c15d78775e25d94e75933def1338
Andrew Hwang <hwanga097@gmail.com>
2023-01-22 21:14:52 -0800

    added random hello strings

--- src/bot_cogs/general.py
+++ src/bot_cogs/general.py
@@ -1,8 +1,15 @@
 import discord
 from discord.ext import commands
-from discord.ext import commands
+from bot_abstractions.constants import BotMessages, BotCommands
+import random
 
 
 class General(commands.Cog):
-    #TODO: add general commands here
-    pass
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(BotCommands.Hello)
+    async def hi(self, ctx):
+        index = random.randint(0, len(BotMessages.HELLO_STRINGS) - 1)
+        await ctx.send(BotMessages.HELLO_STRINGS[index])
+        

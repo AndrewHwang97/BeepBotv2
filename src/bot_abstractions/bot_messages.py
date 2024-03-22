d51cdb23f0124fb3b4d1699e1b8d007d2d9798dc
Andrew Hwang <hwanga097@gmail.com>
2024-03-21 21:30:02 -0700

    add fight cog

--- src/bot_abstractions/bot_messages.py
+++ src/bot_abstractions/bot_messages.py
@@ -54,3 +54,38 @@ class EightBallResponses:
         'Maybe',
         'Try Again'
     ]
+
+class FightResponses:
+    WIN_RESPONSES = [
+        '{0} delivers a devastating uppercut, securing victory!',
+        '{0} outmaneuvers {1} with lightning-fast strikes!',
+        "{0}'s unyielding determination triumphs in the arena!",
+        "{0}'s strategy pays off, leaving {1} defeated!",
+        '{0} lands a critical blow, emerging as the champion!',
+        "{0}'s resilience and skill overcome all obstacles!",
+        '{0} executes a flawless combo, claiming victory!',
+        "{0}'s unwavering focus leads to a decisive win!",
+        "{0}'s precision strikes leave {1} unable to counter!",
+        "{0}'s strength proves unmatched, sealing the win!",
+        '{0} stages an epic comeback, emerging victorious!',
+        "{0} counters {1}'s every move, securing the win!",
+        "{0}'s determination shines through, claiming victory!",
+        "{0}'s strategic prowess outplays {1} for the win!",
+        "{0}'s agility and speed overwhelm {1} in battle!",
+        '{0} lands a critical hit, turning the tide in victory!',
+        "{0}'s calculated strikes leave {1} unable to retaliate!",
+        "{0}'s resilience pays off, clinching the victory!",
+        '{0} executes a perfect defense, securing the win!',
+        "{0}'s skillful maneuvers lead to a well-deserved win!"
+    ]
+    TIE_RESPONSES = [
+        'Both fighters pause for tea, calling it a draw!',
+        'A sudden dance-off erupts; both are equally skilled!',
+        '{0} and {1} decide to form a superhero duo instead!',
+        'The crowd demands a rematch, unable to pick a winner!',
+        '{0} and {1} high-five, agreeing that friendship wins!',
+        '{0} and {1} exchange compliments, ending in a draw!',
+        "A swarm of butterflies interrupts; it's a tie by nature!",
+        "The arena lights flicker; it's a sign of a tie game!",
+        '{0} and {1} declare mutual respect, calling it even!'
+    ]
